6401ab5f70982138b9f8f119143fb614fcf1a3d8
Sarah Elmendorf <selmendorf@neoninc.org>
2015-10-12 03:33:22 -0600

    added reading of csvs

--- scripts/readOriginalExcelFile.R
+++ scripts/readOriginalExcelFile.R
@@ -7,15 +7,26 @@
 #ISSUES - can't read the new plotData
 
 library (xlsx)
-siteData<-read.xlsx('Background/GTREE_Master_30Sept2015.xlsx',sheetName='SiteData',
-                    fileEncoding='latin1')
+
+#something in the 10/11/2015 file won't read, manually converted to csv
+# siteData<-read.xlsx('Background/MASTER_GTREE_11Oct2015.xlsx',sheetName='SiteData',
+#                     fileEncoding='latin1')
+siteData<-read.csv('Background/MASTER_GTREE_11Oct2015_site.csv',
+                                         fileEncoding='latin1')                    
+
+#not always necessary but keeping these because sometimes Andrew's files read with blanks
 siteData<-siteData[siteData$site!='',]
 siteData<-siteData[!is.na(siteData$site),]
-plotData<-read.xlsx('Background/MASTER_GTREE_11Oct2015.xlsx',sheetName='Sheet1',
-                    fileEncoding='latin1')
+
+# 
+# plotData<-read.xlsx('Background/MASTER_GTREE_11Oct2015.xlsx',sheetName='PlotData',
+#                     fileEncoding='latin1')
+plotData<-read.csv('Background/MASTER_GTREE_11Oct2015_plot.csv',
+                   fileEncoding='latin1')  
 plotData<-plotData[plotData$site!='',]
 plotData<-plotData[!is.na(plotData$site),]
 
+unique(plotData$nat.seedling.sp.y0)  # some weirdness here still
 
 source('scripts/rem_specialChars.R')
 siteData<-rem_specialChars(siteData, cols=names(siteData))
