4deb52cf4bdc861cce8c9a0a72a78895308f4a62
Sarah Elmendorf <selmendorf@neoninc.org>
2015-10-13 15:13:14 -0600

    subsetPlot modified to include all species, all sites for now

--- scripts/subsetPlotData.R
+++ scripts/subsetPlotData.R
@@ -9,13 +9,15 @@
 plotData <-read.csv ('Data/cleanData/plotDataClean.csv')
 siteData <-read.csv ('Data/cleanData/siteDataClean.csv')
 
-plotDatasub<-plotData[plotData$site %in% c("Churchill, MB","Canol Trail, NWT","Wolf Creek, YK")==FALSE,]
+plotDatasub<-plotData
+#TURN ON THIS LINE TO GET RID OF THE DOUBLE PLANTED OR OTHERWISE DEAL WITH IT
+#plotDatasub<-plotData[plotData$site %in% c("Churchill, MB","Canol Trail, NWT","Wolf Creek, YK")==FALSE,]
 
 #extract only ONE SPECIES for now
-myspp<-as.data.frame(unique(siteData[,c("site","sp.seeded")]))
-myspp<-myspp[!duplicated(myspp$site),]
-#this will be called exp.seedling.sp
-plotDatasub<-merge(plotDatasub,myspp,by.x=c("site","exp.seedling.sp"),by.y=c("site","sp.seeded")) #!!!
+# myspp<-as.data.frame(unique(siteData[,c("site","sp.seeded")]))
+# myspp<-myspp[!duplicated(myspp$site),]
+# #this will be called exp.seedling.sp
+# plotDatasub<-merge(plotDatasub,myspp,by.x=c("site","exp.seedling.sp"),by.y=c("site","sp.seeded")) #!!!
 
 #Unique plot names per plot
 plotDatasub$uniquePlot<-paste(plotDatasub$site,plotDatasub$zone,plotDatasub$transect,plotDatasub$plot,sep="_")
@@ -40,12 +42,5 @@ plotDatasub<-plotDatasub[c(plotDatasub$site=="Davos" & c(plotDatasub$start.year=
 
 #AT only
 plotDatasub<-plotDatasub[plotDatasub$zone=="AT",]
-
-
-#list of unique plots with the associated site number also with scarT 
-
-#make up germination rate data but replace this will real data later
-germRate<-setNames(as.data.frame(cbind(c(seq(1,length(unique(plotDatasub$site)),by=1)),c(runif(length(unique(plotDatasub$site)),0.8,1)))),c("siteNum","germRate"))
-
 
 write.csv(plotDatasub, 'data/subsets/plotDatasub.csv', row.names=F)
